57f0b5b1192708e451238673a1577c9211543379
praveen-osits <praveen.osits@gmail.com>
2018-04-27 12:24:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var passport= require('passport');
+var session = require('express-session');
+var connectMongo = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+var uid = require('uid-safe');
+require('./models/Posts');
+require('./models/Comments');
+var packageJson = require('./package.json');
+
+
+require('./models/User');
+require('./config/passport');
+
+var genuuid = function(){
+  return uid.sync(18);
+}
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//mongoose.connect('mongodb://localhost/news');
+//mongoose.connect('mongodb.services.clever-cloud.com:27017/bq608aruh0oisb3',{user:'unohyoyqbewibhr',pass:'AfD4l6dxQyDlEuqoFZt6'});
+mongoose.connect(packageJson.mongodbUrl);
+console.log("initializing the sessions..");
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+app.use(session({
+  store: new connectMongo({
+    mongooseConnection:mongoose.connection,
+    ttl:(5*60*60)
+
+  }),
+  secret:'MEANapp',
+  saveUninitialized:true,
+  resave:false,
+  cookie:{
+    path:"/",
+    httpOnly:true,
+    maxAge:5*60*1000,
+  },
+  name:"MEAN_APP",
+  genid:function(req){
+    return genuuid()
+  }
+
+}))
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,160 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
+var jwtPermission = require('express-jwt-permissions');
+var auth = jwt({secret:'SECRET', userProperty:'payload'});
+var guard = jwtPermission({
+   requestProperty: 'payload',
+   permissionsProperty:'permissions'
+})
+require('../models/Posts');
+require('../models/Comments');
+require('../models/User');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+router.post('/posts', auth,guard.check('write'), function(req, res, next) {
+  var post = new Post(req.body);
+  post.author = req.payload.username;
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.param('comment', function(req, res, next, id){
+  //cosole.log(id);
+ var query = Comment.findById(id);
+ query.exec(function (err,comment){
+   if(err) {return next(err);}
+   if(!comment) {return next(new Error('can\'t find comment'));}
+   req.comment = comment;
+   return next();
+ });
+
+});
+
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments',function(err,post){
+    res.json(req.post)
+  });
+
+});
+
+router.put('/posts/:post/upvote', auth, guard.check('update'), function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req,res,next){
+  //  console.log("hello");
+    req.comment.upvote(function(err,comment){
+      if(err) {return next(err);}
+
+      res.json(comment);
+    });
+
+
+});
+
+
+
+router.post('/posts/:post/comments', auth, function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.author = req.payload.username;
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+
+router.post('/register',function(req,res,next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message:'Please fill out all fields.'});
+  }
+
+  var roles = ['read','write','update'];
+  var user = new User();
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+  user.role = roles;
+  user.save(function(err){
+    if(err){return next(err);}
+
+    return res.json({token:user.generateJWT()})
+  });
+});
+
+
+router.post('/login',function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message:'Please fill out all fields.'}) ;
+  }
+  passport.authenticate('local',function(err,user,info){
+    if(err){return next(err);}
+    if(user){
+      req.session.save(function(err){
+        if(err)
+        console.log("Error while saving session");
+        console.log('Session saved!');
+      })
+      return res.json({token:user.generateJWT()});
+    }else{
+      return res.status(401).json(info);
+    }
+  })(req,res,next);
+});
+
+router.post('/logout', auth, function(req, res, next){
+  console.log(req.session);
+  if(req.session){
+   req.session.destroy(function(err){
+     if(err) console.log('Error while destroying session');
+     console.log("Session destroyed successfully..");
+     res.status(200).end();
+   })
+  } else{
+   res.status(501).end();
+  }
+});
+
+module.exports = router;
